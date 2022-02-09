ff59c7bd9d0fdae8d1003b4d138842071eef6969
unknown <turbonium335i@gmail.com>
2022-02-09 23:05:11 +0900

    first golf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  NavDropdown,
+  Nav,
+  Container,
+  Row,
+  Col,
+  ProgressBar,
+  Table,
+  Alert,
+} from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Products from "./Pages/Products";
+import { BsFillCartFill } from "react-icons/bs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Container>
+            <Link to="/" className="text-decoration-none">
+              <Navbar.Brand>
+                <span className="text-warning ">OnWear </span>Golf
+              </Navbar.Brand>
+            </Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link to="/" className="text-decoration-none nav-link">
+                  {" "}
+                  ShowCase
+                </Link>
+                <Link to="/memo" className="text-decoration-none nav-link">
+                  {" "}
+                  Kart
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+
+            <BsFillCartFill className="text-light" />
+          </Container>
+        </Navbar>
+        <br />
+        <Products />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+document.body.style = "background: fff;";
---
+++ src/Pages/Products.jsx
@@ -0,0 +1,126 @@
+import {
+  Table,
+  Card,
+  Button,
+  Col,
+  Row,
+  CardGroup,
+  Carousel,
+} from "react-bootstrap";
+
+const Products = () => {
+  return (
+    <div className="container">
+      <CardGroup>
+        <Card>
+          <Card.Img
+            variant="top"
+            src="https://i.postimg.cc/KzKXjnqV/gfore.jpg"
+          />
+          <Card.Body>
+            <Card.Title>G FORE</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            src="https://i.postimg.cc/DfS1jYhW/pxgwo.jpg"
+          />
+          <Card.Body>
+            <Card.Title>PXG</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            src="https://i.postimg.cc/Pq62ZpW0/titleist.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Titleist</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            src="https://i.postimg.cc/KzKXjnqV/gfore.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Titleist</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      <br />
+
+      <Carousel variant="dark">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.postimg.cc/PJv3dX89/slider.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h5>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.postimg.cc/PJv3dX89/slider.jpg"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h5>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.postimg.cc/PJv3dX89/slider.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h5>Third slide label</h5>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Products;
